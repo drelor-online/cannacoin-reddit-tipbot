6222e30f7dea1871490138a007dbc5998117404c
stains <btjsteens@gmail.com>
2021-08-29 20:26:28 +0200

    Fixed modify method. Removed unneeded methods.

--- src/tipper_sql.py
+++ src/tipper_sql.py
@@ -7,40 +7,28 @@ def add_subreddit(
     subreddit,
     reply_to_comments=True,
     footer="",
-    status="friendly",
+    status="full",
     minimum=PROGRAM_MINIMUM,
 ):
     sub = Subreddit(
-        subreddit=subreddit,
-        reply_to_comments=reply_to_comments,
-        footer=footer,
-        status=status,
-        minimum=PROGRAM_MINIMUM
+        subreddit = subreddit,
+        reply_to_comments = reply_to_comments,
+        footer = footer,
+        status = status,
+        minimum = minimum
     )
     sub.save(force_insert=True)
 
+
 @db.connection_context()
 def modify_subreddit(subreddit, status):
-    Subreddit.update(status=status).where(Subredit.subreddit == subreddit).execute()
+    Subreddit.update(status=status).where(Subreddit.subreddit == subreddit).execute()
+
 
 @db.connection_context()
 def rm_subreddit(subreddit):
     Subreddit.delete().where(Subreddit.subreddit == subreddit).execute()
 
-@db.connection_context()
-def backup_keys():
-    accounts = Account.select(Account.username, Account.address, Account.private_key)
-    with open("../backup", "w") as f:
-        for acct in accounts:
-            f.write(acct.username + "," + acct.address + "," + acct.private_key + "\n")
-
-@db.connection_context()
-def backup_accounts():
-    accounts = Account.select()
-    with open("../backup_accounts", "w") as f:
-        for acct in accounts:
-            # TODO - backup other fields
-            f.write(acct.username + "," + acct.address + "," + acct.private_key + "\n")
 
 @db.connection_context()
 def subreddits():
